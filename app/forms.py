e44cbdc8046c9ea2efb6842c29309a5a0dbb5249
JOAO VICTOR DE MESQUITA CANDIDO DOS SANTOS <joao.santos@embraer.com.br>
2019-03-13 15:17:04 -0300

    Aula 8 - 9:21

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from flask_login import current_user
 from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
 from app.models import User
@@ -48,3 +48,7 @@ class UpdateAccountForm(FlaskForm):
             if user:
                     raise ValidationError(message='That email is taken. Please choose a diferent one!')
 
+class PostForm(FlaskForm):
+        title= StringField('Title', validators=[DataRequired()])
+        content = TextAreaField('Content', validators=[DataRequired()])
+        submit = SubmitField('Post')
